a0db322032e2d9d7f67c824ce7afa92b1f9b1bf5
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2024-10-08 14:56:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const button1 = document.querySelector("#button1");
+const shopCart = document.querySelector("#shop-btn");
+const info = document.querySelector("#learnMore");
+const addCart = [
+  {
+    name: 
+  },
+  {
+    
+  }
+];
 button1.onclick = shopNow;
+shopCart.onclick = addCart
 
+function update (addCart) {
+  shopCart.onclick = addCart[][];
+  button1.onclick = 
+}
 
 function shopNow(){
+  cart();
+  
+}
+
+const cart = () => {
+  addCart();
+  cal();
+}
+
+const checkout = () => {
   
 }
