4ed3f02195ce91e1e86e8b9b59180db539dece63
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2024-10-22 09:15:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const checkoutButton = document.querySelector("#checkout_btn");
 const shopNow = document.querySelector("#shop-btn");
 const cartButton = document.querySelector(".cart_btn");
 const clearButton = document.getElementById("#clear");
+const submitButton = document.getElementById("#submit");
 
 // initializing buttons
 cartButton.onclick = goCheckout;
@@ -36,7 +37,19 @@ const checkout = () => {
 }
 
 const validation () {
-  
+  try {
+    if (submitButton == "") throw "empty";   //error case
+    if (isNaN(submitButton)) throw "not a number";
+    submitButton = Number();
+    if (submitButton >= 10) throw "too high";
+  }
+  catch(err) {
+    document.write("Input is " + err);
+    console.log(err);
+  }
+  finally {
+    document.write("</br  </Done");
+  }
 }
 
 const goCheckout = () => {
